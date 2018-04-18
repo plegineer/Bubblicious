e50b989f70bf9a690e6a9757acce232a112d8c64
Kazuki Shimada <shimada@plegineer.com>
2018-04-18 12:25:40 +0900

    first commit

---
+++ Bubblicious/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Bubblicious
+//
+//  Created by 島田一輝 on 2018/04/18.
+//  Copyright © 2018年 Plegineer Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
