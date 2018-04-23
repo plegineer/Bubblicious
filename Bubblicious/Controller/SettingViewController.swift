559e2df726a67ce092636faae75a2b7d85ddb3b8
Kazuki Shimada <shimada@plegineer.com>
2018-04-23 19:49:40 +0900

    部品制約の追加、pickerViewを削除

--- Bubblicious/Controller/SettingViewController.swift
+++ Bubblicious/Controller/SettingViewController.swift
@@ -8,13 +8,7 @@
 
 import UIKit
 
-class SettingViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    var pickerView = UIPickerView()
-    var label = UILabel()
-    
-    let dataList: [String] = ["hogehoge", "fugafuga", "fobar"]
-//    let customView: UIView
+class SettingViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,39 +21,11 @@ class SettingViewController: UITableViewController, UIPickerViewDelegate, UIPick
         self.navigationItem.rightBarButtonItem = rightButton
         
         let customView = CustomView(frame: self.view.bounds)
+//        customView.translatesAutoresizingMaskIntoConstraints = false
 
         customView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 
         self.view.addSubview(customView)
-        
-        self.view.backgroundColor = UIColor(red: 0.87, green: 1.0, blue: 0.94, alpha: 1.0)
-        
-        // PickerView のサイズと位置
-        pickerView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 300)
-        pickerView.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
-        
-        // PickerViewはスクリーンの中央に設定
-        pickerView.center = self.view.center
-        
-        // Delegate設定
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        
-        // Label 位置はPickerViewより150上に設定、高さを２行表示分とる
-        let yPosition = pickerView.frame.origin.y
-        label.frame = CGRect(x: 0, y: yPosition - 100, width: self.view.frame.width, height: 100)
-        
-        // ラベルを２行表示
-        label.numberOfLines = 2
-        label.text = "pickerView"
-        // フォントサイズを大きく
-        label.font = UIFont.systemFont(ofSize: 36)
-        // テキストを中央寄せにする
-        label.textAlignment = NSTextAlignment.center
-        
-        
-        self.view.addSubview(pickerView)
-        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
@@ -73,31 +39,5 @@ class SettingViewController: UITableViewController, UIPickerViewDelegate, UIPick
         
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         UIApplication.shared.keyWindow?.rootViewController = storyboard.instantiateViewController(withIdentifier: "login") as! UINavigationController
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    // UIPickerViewの行数、リストの数
-    func pickerView(_ pickerView: UIPickerView,
-                    numberOfRowsInComponent component: Int) -> Int {
-        return dataList.count
-    }
-    
-    // UIPickerViewの最初の表示
-    func pickerView(_ pickerView: UIPickerView,
-                    titleForRow row: Int,
-                    forComponent component: Int) -> String? {
-        
-        return dataList[row]
-    }
-    
-    // UIPickerViewのRowが選択された時の挙動
-    func pickerView(_ pickerView: UIPickerView,
-                    didSelectRow row: Int,
-                    inComponent component: Int) {
-        
-        label.text = dataList[row]
     }
 }
--- Bubblicious/CustomView.swift
+++ Bubblicious/CustomView.swift
@@ -8,41 +8,62 @@
 
 import UIKit
 
-class CustomView: UIView, UIPickerViewDelegate {
+class CustomView: UIView, UIPickerViewDelegate, UIPickerViewDataSource {
     
     let titleLabel1: UILabel
-    var textField1: UITextField
+    let titleLabel2: UILabel
+    let titleLabel3: UILabel
+    
+    let textField1: UITextField
+    let textField2: UITextField
+    
     let switchControl: UISwitch
     let button: UIButton
-    let picker: UIPickerView
-    let dataList: [String] = ["hogehoge", "fugafuga", "fobar"]
+    
+    var pickerView: UIPickerView
+    let dataList = ["hogehoge", "fugafuga", "fobar"]
     
     override init(frame: CGRect) {
 
         self.titleLabel1 = UILabel()
-        self.titleLabel1.text = "Hello World!"
+        self.titleLabel1.text = "タイトル1"
         self.titleLabel1.textAlignment = .center
         
+        self.titleLabel2 = UILabel()
+        self.titleLabel2.text = "タイトル2"
+        self.titleLabel2.textAlignment = .center
+        
+        self.titleLabel3 = UILabel()
+        self.titleLabel3.text = "タイトル3"
+        self.titleLabel3.textAlignment = .center
+        
         self.textField1 = UITextField()
-        self.textField1.textAlignment = .center
+        self.textField1.text = "hogehoge"
+        
+        self.textField2 = UITextField()
+        self.textField2.text = "content"
         
         self.switchControl = UISwitch()
-        
+
         self.button = UIButton()
         self.button.setTitle("保存する", for: UIControlState.normal)
         self.button.setTitleColor(UIColor.blue, for: UIControlState.normal)
         
-        self.picker = UIPickerView()
-        self.picker.showsSelectionIndicator = true
+        self.pickerView = UIPickerView()
         
         super.init(frame: frame)
         
-        self.button.addTarget(self,
-                              action: #selector(buttonTapped),
-                              for: .touchUpInside)
+        self.button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+    
         self.backgroundColor = .white
         self.addSubview(titleLabel1)
+        self.addSubview(titleLabel2)
+        self.addSubview(titleLabel3)
         self.addSubview(textField1)
+        self.addSubview(textField2)
         self.addSubview(switchControl)
         self.addSubview(button)
     }
@@ -54,48 +75,56 @@ class CustomView: UIView, UIPickerViewDelegate {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        let labelSize = self.titleLabel1.sizeThatFits(self.bounds.size)
+        let labelSize1 = self.titleLabel1.sizeThatFits(self.bounds.size)
+        let labelSize2 = self.titleLabel2.sizeThatFits(self.bounds.size)
+        let labelSize3 = self.titleLabel3.sizeThatFits(self.bounds.size)
         
-        let x = (self.bounds.width - labelSize.width) / 2
-        let y = (self.bounds.height - labelSize.height) / 2
+        let x = (self.bounds.width - labelSize1.width) / 2
+        let y = (self.bounds.height - labelSize1.height) / 2
         
-        self.titleLabel1.frame = CGRect(origin: CGPoint(x: x, y: y),
-                                        size: labelSize)
-        self.textField1.frame = CGRect(origin: CGPoint(x: x, y: y + 50.0),
-                                       size: labelSize)
-        self.textField1.borderStyle = .roundedRect
-        self.switchControl.frame = CGRect(origin: CGPoint(x: x, y: y + 100.0),
-                                          size: labelSize)
+        self.titleLabel1.frame = CGRect(origin: CGPoint(x: 100, y: y), size: labelSize1)
+        self.titleLabel2.frame = CGRect(origin: CGPoint(x: 100, y: y + 50), size: labelSize2)
+        self.titleLabel3.frame = CGRect(origin: CGPoint(x: 100, y: y + 100), size: labelSize3)
+        
+        let textFieldSize1 = self.textField1.sizeThatFits(self.bounds.size)
+        let textFieldSize2 = self.textField2.sizeThatFits(self.bounds.size)
+        
+        self.textField1.frame = CGRect(origin: CGPoint(x: 150 + labelSize1.width, y: y),
+                                       size: textFieldSize1)
+        self.textField2.frame = CGRect(origin: CGPoint(x: 150 + labelSize2.width, y: y + 50),
+                                       size: textFieldSize2)
+    
+        self.switchControl.frame = CGRect(origin: CGPoint(x: 150 + labelSize3.width, y: y + 100.0),
+                                          size: textFieldSize1)
+        
+        
+        let buttonSize = self.button.sizeThatFits(self.bounds.size)
         self.button.frame = CGRect(origin: CGPoint(x: x, y: y + 150.0),
-                                   size: labelSize)
+                                   size: buttonSize)
     }
     
     @objc func buttonTapped(sender: Any) {
         print("保存しました")
+        print(textField1.text!)
+        print(textField2.text!)
     }
     
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    // UIPickerViewの行数、リストの数
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return dataList.count
     }
     
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        print("titleRorRow")
+    // UIPickerViewの最初の表示
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return dataList[row]
     }
     
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    // UIPickerViewのRowが選択された時の挙動
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int,inComponent component: Int) {
         self.textField1.text = dataList[row]
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
