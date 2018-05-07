39e281cfc199dd01a4465166ebac09c68e56bc30
Kazuki Shimada <shimada@plegineer.com>
2018-05-07 14:03:50 +0900

    カメラ機能使用権限部分の追加

--- Bubblicious/Controller/UploadImageViewController.swift
+++ Bubblicious/Controller/UploadImageViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 protocol UploadImageViewControllerDelegate: class {
     func UploadImageViewController(didFinished view: UploadImageViewController)
@@ -23,33 +24,54 @@ class UploadImageViewController: UIViewController, UIImagePickerControllerDelega
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: #selector(self.pushedBackButton))
         self.title = "アップロード画面"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @objc func pushedBackButton() {
         self.delegate?.UploadImageViewController(didFinished: self)
     }
     
-    @IBAction func pushedOpenAlbumButton(_ sender: Any) {
+    @IBAction func pushedOpenImageButton(_ sender: Any) {
         
-        let actionSheet = UIAlertController(title: "画像を開く", message: "選択してください", preferredStyle: .actionSheet)
-        
-        let saveTo1 = UIAlertAction(title: "ライブラリから選択", style: .default, handler: actionSheetChoose(sender:))
-        let saveTo2 = UIAlertAction(title: "写真を撮る", style: .default, handler: actionSheetChoose(sender:))
-        let cancel = UIAlertAction(title: "キャンセル", style: .cancel, handler: {
-            (action: UIAlertAction) -> Void in
-            print("キャンセルしました")
+        let controller = UIAlertController(title: "画像を開く", message: "選択してください", preferredStyle: .actionSheet)
+        let actionLibrary = UIAlertAction(title: "ライブラリから選択", style: .default, handler: {(action: UIAlertAction) in
+            self.pickImageFromLibrary()
+            
         })
+        let actionCamera = UIAlertAction(title: "写真を撮る", style: .default, handler: {(action: UIAlertAction) in
+            self.pickImageFromCamera()
+            
+        })
+        let actionCancel = UIAlertAction(title: "キャンセル", style: .cancel, handler: {(action: UIAlertAction) in
+            // do nothing
+        })
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            controller.addAction(actionLibrary)
+        }
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            controller.addAction(actionCamera)
+        }
         
-        actionSheet.addAction(saveTo1)
-        actionSheet.addAction(saveTo2)
-        actionSheet.addAction(cancel)
-        
-        present(actionSheet, animated: true, completion: nil)
-
+        let status = PHPhotoLibrary.authorizationStatus()
+        if status == PHAuthorizationStatus.denied {
+            let alert = UIAlertController(title: "ライブラリのアクセス権限が必要です", message: "設定画面で設定して下さい", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {(action: UIAlertAction) in }))
+            showAlert("位置情報サービスの設定が「無効」になっています", message: "設定 > プライバシー > 位置情報サービス で、位置情報サービスの利用を許可して下さい")
+        }
+        controller.addAction(actionCancel)
+        present(controller, animated: true, completion: nil)
+    }
+    
+    private func pickImageFromLibrary() {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    private func pickImageFromCamera() {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = UIImagePickerControllerSourceType.camera
+        self.present(controller, animated: true, completion: nil)
     }
     
     func imagePickerController(_ imagePicker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -58,26 +80,5 @@ class UploadImageViewController: UIViewController, UIImagePickerControllerDelega
             imageView.image = pickedImage
         }
         imagePicker.dismiss(animated: true, completion: nil)
-    }
-    
-    func actionSheetChoose(sender: UIAlertAction) {
-        
-        switch sender.title {
-        case "ライブラリから選択":
-            let imagePickerController = UIImagePickerController()
-            imagePickerController.delegate = self
-            _ = UIImagePickerControllerSourceType.photoLibrary
-            self.present(imagePickerController, animated:true, completion:nil)
-        case "写真を撮る":
-            let imagePickerController = UIImagePickerController()
-            imagePickerController.delegate = self
-            imagePickerController.sourceType = .camera
-            self.present(imagePickerController, animated:true, completion:nil)
-        case "キャンセル":
-            print("キャンセルしました")
-        default:
-            return
-        }
-        print("\(sender.title!)に保存しました")
     }
 }
