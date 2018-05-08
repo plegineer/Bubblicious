9874d9a673af115b80e3aa8db647f2f0835e704f
Kazuki Shimada <shimada@plegineer.com>
2018-05-08 10:55:45 +0900

    ExpandableCell用tableViewの作成

---
+++ Bubblicious/Controller/ExpandViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ExpandViewController.swift
+//  Bubblicious
+//
+//  Created by 島田一輝 on 2018/05/08.
+//  Copyright © 2018年 Plegineer Inc. All rights reserved.
+//
+
+import UIKit
+
+class ExpandViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
