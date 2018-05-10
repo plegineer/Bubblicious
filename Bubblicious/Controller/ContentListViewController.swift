536fddfc5356cccb2638797077452b97d87da8bd
Yoshiki Agatsuma <agatsuma@plegineer.com>
2018-05-10 18:23:48 +0900

    ContentModelクラスに処理を切り出し

--- Bubblicious/Controller/ContentListViewController.swift
+++ Bubblicious/Controller/ContentListViewController.swift
@@ -13,16 +13,15 @@ class ContentListController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    private var contents: [ContentData] = []
-    private var extraContents: [ContentData] = []
-    private var isAppendedExtra: Bool = false
+    private let contentModel = ContentModel()
+    private let tempRequestParam: [String: Int] = ["temp": 1]
     
     private var footerIndicatorView: UIActivityIndicatorView!
     
-    struct Identifier {
-        static let basicCell = "basicCell"
-        static let customCell = "contentCustomCell"
-    }
+    // APIの呼び出しタイミング
+    //  ・viewWillAppear()
+    //  ・pullToRefresh
+    //  ・一番下までスクロール時
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,68 +37,21 @@ class ContentListController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    private func requestToGetContents() {
-        // 本来であれば、APIを叩き、表示するデータを取得する
-        // 今回は、固定のデータを利用し、プログラム内で配列を2つに分け、表示を出し分けしている
-        
-        let tempData: [[String]] = [
-            ["https://xxxx/yyyy/hoge.png", "タイトル", ""],
-            ["https://xxxx/yyyy/hoge.png", "タイトル", ""],
-            ["https://xxxx/yyyy/hoge.png", "タイトル", ""],
-            ["https://xxxx/yyyy/hoge.png", "タイトル", ""],
-            ["", "タイトル", ""],
-            ["", "タイトル", ""],
-            ["", "タイトル", ""],
-            ["", "タイトル", "カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容"],
-            ["", "タイトル", "カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容"],
-            ["", "タイトル", "カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容"],
-            ["https://xxxx/yyyy/hoge.png", "タイトル", ""],
-            ["https://xxxx/yyyy/hoge.png", "タイトル", ""],
-            ["https://xxxx/yyyy/hoge.png", "タイトル", ""],
-            ["https://xxxx/yyyy/hoge.png", "タイトル", ""],
-            ["", "タイトル", ""],
-            ["", "タイトル", ""],
-            ["", "タイトル", ""],
-            ["", "タイトル", "カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容"],
-            ["", "タイトル", "カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容"],
-            ["", "タイトル", "カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容カスタムセルの内容"],
-            ["", "(追加読み込み分)タイトル", ""], // ↓ここから追加読み込み分
-            ["", "(追加読み込み分)タイトル", ""],
-            ["", "(追加読み込み分)タイトル", "カスタムセルの内容"],
-            ["https://xxxx/yyyy/hoge.png", "(追加読み込み分)タイトル", ""],
-            ["https://xxxx/yyyy/hoge.png", "(追加読み込み分)タイトル", ""],
-            ["", "(追加読み込み分)タイトル", ""],
-            ["", "(追加読み込み分)タイトル", ""],
-            ["", "(追加読み込み分)タイトル", "カスタムセルの内容"],
-            ["https://xxxx/yyyy/hoge.png", "(追加読み込み分)タイトル", ""],
-            ["https://xxxx/yyyy/hoge.png", "(追加読み込み分)タイトル", ""]
-        ]
-        
-        self.contents.removeAll()
-        self.extraContents.removeAll()
-        
-        for (index, datum) in tempData.enumerated() {
-            let title = datum[1] + String(index+1)
-            let param = self.createTempContentParam(imageUrl: datum[0], title: title, description: datum[2])
-            if index < 20 {
-                // 初期表示分の配列
-                self.contents.append(ContentData(data: param))
-            } else {
-                // 追加読み込み分の配列
-                self.extraContents.append(ContentData(data: param))
+    private func requestToGetContents(isFromRefresh: Bool = false) {
+        // APIリクエスト処理(初期表示アイテム取得)
+        self.contentModel.getContents(tempRequestParam) { (error) in
+            if let error = error {
+                // 今回は絶対に通らない
+                self.showAlert("UnExpected Error", message: error.localizedDescription)
             }
+            
+            if isFromRefresh {
+                self.tableView.refreshControl?.endRefreshing()
+            }
+            
+            self.contentModel.isAppendedExtra = false
+            self.tableView.reloadData()
         }
-        
-        self.isAppendedExtra = false
-        self.tableView.reloadData()
-    }
-    
-    private func createTempContentParam(imageUrl: String, title: String, description: String) -> [String: String] {
-        var param: [String: String] = [:]
-        param[ContentData.ParamKey.imageUrl] = imageUrl
-        param[ContentData.ParamKey.title] = title
-        param[ContentData.ParamKey.description] = description
-        return param
     }
     
     private func setRefreshController() {
@@ -109,34 +61,28 @@ class ContentListController: UIViewController {
     }
     
     @objc func doRefresh() {
-        self.requestToGetContents()
-        
-        // 視覚的にわかりやすいよう、故意に1秒待つ処理を入れている
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.tableView.refreshControl?.endRefreshing()
-        }
+        self.requestToGetContents(isFromRefresh: true)
     }
-    
 }
 
 extension ContentListController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.contents.count
+        return self.contentModel.contents.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let content = self.contents[indexPath.row]
+        let content = self.contentModel.contents[indexPath.row]
         
         var cell: UITableViewCell
         if content.description.isEmpty {
             cell = tableView.dequeueReusableCell(withIdentifier: "basicCell")!
             
             if content.imageUrl.isEmpty {
-                // 画像なしのベーシックセルの設定
+                // 画像なしベーシックセルの設定
                 cell.textLabel?.text = content.title
             } else {
-                // 画像ありのベーシックセルの設定
+                // 画像ありベーシックセルの設定
                 cell.textLabel?.text = content.title
                 
                 // 本来ならば、imageUrlを(String->URL)に変換して、画像を取得し、取得した画像を表示させる(画像取得の際のエラーハンドリングは別途必要)
@@ -145,7 +91,7 @@ extension ContentListController: UITableViewDataSource {
             }
             
         } else {
-            // 画像なしのカスタムセル
+            // 画像なしカスタムセルの設定
             let customCell = tableView.dequeueReusableCell(withIdentifier: "contentCustomCell") as! ContentCustomTableViewCell
             customCell.titleLabel.text = content.title
             customCell.descriptionLabel.text = content.description
@@ -179,20 +125,19 @@ extension ContentListController: UITableViewDelegate {
 
 extension ContentListController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
         // 最下部までスクロールしたかどうか
         if tableView.contentOffset.y + tableView.frame.size.height > tableView.contentSize.height {
-            
-            // 本来であれば、フラグで管理するのではなく、追加で取得出来る件数があるか、などの判定式を利用する
-            // 今回は、追加分の配列(extraContents)を1度だけ、表示させる配列(contents)に追加する
-            if !isAppendedExtra {
-                self.footerIndicatorView.isHidden = false
+            // 追加アイテムの取得は1度のみ
+            if !self.contentModel.isAppendedExtra {
+                self.contentModel.isAppendedExtra = true
                 self.footerIndicatorView?.startAnimating()
-                self.isAppendedExtra = true
                 
-                // 視覚的にわかりやすいよう、故意に1秒待つ処理を入れている
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                    self.contents.append(contentsOf: self.extraContents)
+                // APIリクエスト処理(追加アイテム取得)
+                self.contentModel.getMoreContents(tempRequestParam) { (error) in
+                    if let error = error {
+                        // 今回は絶対に通らない
+                        self.showAlert("UnExpected Error", message: error.localizedDescription)
+                    }
                     self.tableView.reloadData()
                     self.footerIndicatorView.stopAnimating()
                 }
