45de67400ff54e3d8156e941d4a931f2df501d6b
Yoshiki Agatsuma <agatsuma@plegineer.com>
2018-05-10 12:13:43 +0900

    軽微な修正

--- Bubblicious/Controller/ContentListViewController.swift
+++ Bubblicious/Controller/ContentListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  ContentListController.swift
 //  Bubblicious
 //
 //  Created by 島田一輝 on 2018/04/18.
@@ -8,17 +8,15 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+// リスト(UITableView)系のサンプルコントローラークラス
+class ContentListController: UIViewController {
     
-    @IBOutlet weak var logoutButton: UIButton!
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
